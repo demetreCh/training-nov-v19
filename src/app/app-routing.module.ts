25382d6fac8ce023f6a70a14e2fb798bad4d586e
demetreCh <tri2000@gmx.net>
2024-11-13 15:07:28 +0000

    Routage : wip 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './post/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {
+    path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path: 'posts', loadChildren: ()=>import('./post/post.module').then(m=> m.PostModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyLibComponent } from 'my-lib';
+import { DashboardComponent } from './post/dashboard/dashboard.component';
+import { PostsComponent } from './post/posts/posts.component';
+import { PostDetailComponent } from './post/post-detail/post-detail.component';
+import { AuthorDetailComponent } from './post/author-detail/author-detail.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    MyLibComponent,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
